fa74deaa082d33a4823bb59ec0b1747ddee29afd
Nikita <anisimoff.nikita@gmail.com>
2018-06-24 00:02:15 +0300

    unification algorithm

--- src/Prolog/Unification.hs
+++ src/Prolog/Unification.hs
@@ -1,26 +1,64 @@
+{-# LANGUAGE TypeOperators #-}
 module Prolog.Unification where
 
 import           Prolog.Syntax
 import           Data.List                      ( find )
 import           Control.Monad                  ( zipWithM )
+import           Debug.Trace
 
+type Checks = [Check]
+type Results = [Result]
 
-unification :: Term -> Term -> Maybe [(Term, Term)]
-unification t1 t2 = case (t1, t2) of
-  (VariableTerm Anonymous    , _                         ) -> Just []
-  (VariableTerm (Named n  i ), AtomTerm x                ) -> Just [(t1, t2)]
-  (VariableTerm (Named n  i ), NumberTerm x              ) -> Just [(t1, t2)]
-  (VariableTerm (Named n1 i1), VariableTerm (Named n2 i2)) -> Just [(t1, t2)]
-  (_                         , VariableTerm (Named n i)  ) -> unification t2 t1
+data Result = Term := Term
+data Check = Term :? Term
+
+unification :: Checks -> Results -> Maybe Results
+unification []              work = Just work
+unification (t :? p : rest) work = do
+  (stack', work') <- unification' t p
+  case work' of
+    Just (t := p) -> do
+      updatedStack <- updateEquals t p rest
+      updatedWork  <- updateResult t p work
+      unification updatedStack (t := p : updatedWork)
+    Nothing -> unification (stack' ++ rest) work
+
+updateEquals :: Term -> Term -> Checks -> Maybe Checks
+updateEquals t p equals = Just $ map f equals
+  where f (t' :? p') = replaceOccurrence t p t' :? replaceOccurrence t p p'
+
+updateResult :: Term -> Term -> Results -> Maybe Results
+updateResult t p results = concat <$> mapM f results
+ where
+  f x@(t' := p') =
+    let t'' = replaceOccurrence t p t'
+        p'' = replaceOccurrence t p p'
+    in  if t' == t'' && p' == p'' then Just [x] else unification [t'' :? p''] []
+
+replaceOccurrence :: Term -> Term -> Term -> Term
+replaceOccurrence t q (CompoundTerm f args) = CompoundTerm f (map update args)
+ where
+  update p@(CompoundTerm f args) = replaceOccurrence t q p
+  update p                       = if t == p then q else p
+replaceOccurrence t q p = if t == p then q else p
+
+unification' :: Term -> Term -> Maybe (Checks, Maybe Result)
+unification' t p = case (t, p) of
+  (Cut                     , _           ) -> Just ([], Nothing)
+  (VariableTerm Anonymous  , _           ) -> Just ([], Nothing)
+  (VariableTerm (Named n i), AtomTerm x  ) -> Just ([], Just (t := p))
+  (VariableTerm (Named n i), NumberTerm x) -> Just ([], Just (t := p))
+  (VariableTerm (Named n1 i1), VariableTerm (Named n2 i2)) ->
+    Just ([], Just (t := p))
+  (_, VariableTerm (Named n i)) -> unification' p t
   (VariableTerm (Named n i), CompoundTerm f args) -> case find (f i) args of
     Just _  -> Nothing
-    Nothing -> Just [(t1, t2)]
+    Nothing -> Just ([], Just (t := p))
    where
     f i (VariableTerm (Named _ j)) = i == j
     f i _                          = False
-
   (CompoundTerm f1 args1, CompoundTerm f2 args2) ->
     if f1 == f2 && length args1 == length args2
-      then concat <$> zipWithM unification args1 args2
+      then Just (zipWith (:?) args1 args2, Nothing)
       else Nothing
-  (t1, t2) -> if t1 == t2 then Just [] else Nothing
+  (t, p) -> if t == p then Just ([], Nothing) else Nothing
